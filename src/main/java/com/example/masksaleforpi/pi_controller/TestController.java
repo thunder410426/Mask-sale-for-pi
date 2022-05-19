6d2108a618c980655788f0dc7ddddc493b36f766
thunder410426 <1164410836@qq.com>
2022-05-19 23:16:21 +0800

    加引脚监听器，修改引脚位置

--- src/main/java/com/example/masksaleforpi/pi_controller/TestController.java
+++ src/main/java/com/example/masksaleforpi/pi_controller/TestController.java
@@ -1,7 +1,10 @@
 package com.example.masksaleforpi.pi_controller;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.io.gpio.impl.GpioPinImpl;
+import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,13 +31,35 @@ public class TestController {
         System.out.println("Started");
         GpioController gpio = GpioFactory.getInstance();
         GpioPinPwmOutput motor;
-        final GpioPinPwmOutput p = gpio.provisionSoftPwmOutputPin(RaspiBcmPin.GPIO_15, "motor", 50);
+
+        GpioPinDigitalInput button = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, "button",
+                PinPullResistance.PULL_DOWN);
+
+        //获取与此按钮关联的 GPIO 引脚的显式状态枚举
+        PinState state = button.getState();
+        // 使用方便的包装方法来审问的按钮状态
+        boolean buttonPressed = button.isHigh();
+        button.addListener(new GpioUsageExampleListener());
+
+
+        //输出
+        final GpioPinPwmOutput p = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_01, "motor", 50);
         p.setShutdownOptions(true, PinState.LOW);
         p.setPwmRange(100);
+
+        //pin关机
+        p.setShutdownOptions(true, PinState.LOW, PinPullResistance.PULL_DOWN);
+        //添加触发器
+        button.addTrigger(new GpioSyncStateTrigger((GpioPinDigitalOutput) p));
+
+
         while (true){
             p.setPwm(200);
             p.setPwm(200);
-            Thread.sleep(1000);
+
+
+            Thread.sleep(10000);
+
             System.out.println("旋转！旋转！");
         }
 
@@ -63,17 +88,6 @@ public class TestController {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 //
 //    // create gpio controller instance
 //    final GpioController gpio = GpioFactory.getInstance();
@@ -95,4 +109,13 @@ public class TestController {
 //        return "close";
 //    }
 
+}
+class GpioUsageExampleListener implements GpioPinListenerDigital {
+    @Override
+    public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+        // 在控制台上显示 pin 状态
+        System.out.println(
+                " --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+
+    }
 }
